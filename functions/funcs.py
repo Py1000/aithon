0253a94c29e3ea762be09cf73505590720eb9c24
Py1000 <89375743+Py1000@users.noreply.github.com>
2022-03-20 11:29:30 +0530

    Update funcs.py

--- functions/funcs.py
+++ functions/funcs.py
@@ -1,4 +1,19 @@
 import os
 
 def setup():
-  
+  try:
+    varFile = open("/storage/emulated/0/VARS/var.txt","r")
+    varFile = varFile.read()
+  except:
+    os.system("termux-setup-storage")
+    os.mkdir("/storage/emulated/0/VARS")
+    varFile = open("/storage/emulated/0/VARS/var.txt","a")
+    API_ID = input("Enter API_ID: ")
+    varFile.write(f"{API_ID} ")
+    API_HASH = input("Enter API_HASH: ")
+    varFile.write(f"{API_HASH} ")
+    SESSION = input("Enter SESSION: ")
+    varFile.write(f"{SESSION}")
+    varFile = open("/storage/emulated/0/VARS/var.txt","r")
+    varFile = varFile.read()
+
