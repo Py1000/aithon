c79d083d6c8cb64fa5b201dbe160b49aeb419eb1
Py1000 <89375743+Py1000@users.noreply.github.com>
2022-03-20 11:37:30 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -4,6 +4,12 @@ from telethon import TelegramClient
 import functions import *
 
 setup()
+varFileS = varFile.split(" ")
+API_ID = varFileS[0]
+API_HASH = varFileS[1]
+SESSION = varFileS[2]
+session = str(SESSION)
+
 login()
 
 user = TelegramClient("session",API_ID,API_HASH)
