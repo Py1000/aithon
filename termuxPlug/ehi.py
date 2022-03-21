3200fd5456af0bb03521d85b4ad8bee74220ad83
Py1000 <89375743+Py1000@users.noreply.github.com>
2022-03-21 19:59:49 +0530

    Update ehi.py

--- termuxPlug/ehi.py
+++ termuxPlug/ehi.py
@@ -11,7 +11,7 @@ async def ehi(event):
       eHiformat = f"""
 {e}■■■{e}
 {e}■■■{e}
-{e}{e}{e}{e}
+ {e}{e}{e}{e}
 {e}■■■{e}
 {e}■■■{e}
 """
@@ -22,7 +22,7 @@ async def ehi(event):
       eHiformat = f"""
 {e}■■■{e}
 {e}■■■{e}
-{e}{e}{e}{e}
+ {e}{e}{e}{e}
 {e}■■■{e}
 {e}■■■{e}
 """
