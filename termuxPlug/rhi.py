3317a388f1d5444419c26d943a9eab5615c72ba4
Py1000 <89375743+Py1000@users.noreply.github.com>
2022-03-21 19:33:40 +0530

    Update rhi.py

--- termuxPlug/rhi.py
+++ termuxPlug/rhi.py
@@ -4,6 +4,7 @@
 @admin_cmd()
 async def rhi(event):
   id = event.chat_id
+  rHiMessage = random.choice(rString)
   try:
     await event.edit(rHiMessage)
     print("\n Command Used : rhi \nLocation : {id} \n")
