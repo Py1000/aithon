5a24db943c8c1cc2a32e34d3e199edf67e8816a2
Py1000 <89375743+Py1000@users.noreply.github.com>
2022-03-21 14:47:58 +0530

    Update __main__.py

--- termuxMain/__main__.py
+++ termuxMain/__main__.py
@@ -3,12 +3,18 @@ import os
 os.system("cd")
 os.system("cd aithon")
 
+
 def load(plug):
   f = open(f"/data/data/com.termux/files/home/aithon/termuxPlug/{plug}.py","r")
   rawCode = f.read()
   rawCodeS = rawCode.split("□")
   cmd = rawCodeS[1]
   mainCode = rawCode.split("#")
-  print(mainCode)
+  codes = mainCode[1]
+  cmdLine = codes.replace("@admin_cmd()",f"@user.on(events.NewMessage(pattern=".{cmd}",outgoing = True))")
+  os.mkdir("/data/data/com.termux/files/home/aithon/termuxOp")
+  mFile = open("/data/data/com.termux/files/home/aithon/termuxOp/__main__.py","a")
+  mfile.write(f"")
+  
 
 load("hello")
