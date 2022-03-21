2f88407a9f7b6e49c39d029926b30737ef96248e
Py1000 <89375743+Py1000@users.noreply.github.com>
2022-03-21 14:21:29 +0530

    Update __main__.py

--- termuxMain/__main__.py
+++ termuxMain/__main__.py
@@ -6,7 +6,7 @@ os.system("cd aithon")
 def load(plug):
   f = open(f"/data/data/com.termux/files/home/aithon/termuxPlug/{plug}.py","r")
   lul = f.read()
-  tep = lul.split("#")
+  tep = lul.split("□")
   print(tep)
 
 load("hello")
