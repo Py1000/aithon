b123190b748828bdd000218257cfb735ae412b95
Py1000 <89375743+Py1000@users.noreply.github.com>
2022-03-21 15:01:02 +0530

    Update __main__.py

--- termuxMain/__main__.py
+++ termuxMain/__main__.py
@@ -14,7 +14,10 @@ def load(plug):
   cmdLine = codes.replace("@admin_cmd()",f"@user.on(events.NewMessage(pattern=".{cmd}",outgoing = True))")
   os.mkdir("/data/data/com.termux/files/home/aithon/termuxOp")
   mFile = open("/data/data/com.termux/files/home/aithon/termuxOp/__main__.py","a")
-  mfile.write(f"")
+  print(cmdLine)
+  mFile.write(f"\n\n\n {cmdLine}")
   
 
 load("hello")
+fileOp = open("/data/data/com.termux/files/home/aithon/termuxOp/__main__.py","a")
+fileOp.write("user.run_until_disconnected()")
