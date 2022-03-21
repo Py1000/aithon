22973f85f8987ad5a89d3bdd5ff37ecbe65f478e
Py1000 <89375743+Py1000@users.noreply.github.com>
2022-03-21 18:58:31 +0530

    Update del.py

--- termuxPlug/del.py
+++ termuxPlug/del.py
@@ -3,6 +3,11 @@
 #
 @admin_cmd()
 async def del(event):
-  await toDel = event.get_reply_message()
-  await toDel.delete()
-  await event.delete()
+  id = event.chat_id
+  try:
+    toDel =  await event.get_reply_message()
+    await toDel.delete()
+    await event.delete()
+    print(f"Command used : del \nLocation : {id}")
+  except:
+    event.edit("**ERROR OCCURRED** \nDo: ```.del <reply a message>```")
