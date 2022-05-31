97fa837cd8db5fc931e5dfbcedd57e0d367dc418
Py1000 <89375743+Py1000@users.noreply.github.com>
2022-05-31 17:28:51 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -2,7 +2,6 @@ import os
 import termuxMain
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-from functions import *
 
 os.system("python /data/data/com.termux/files/home/aithon/termuxMain")
 os.system("python /data/data/com.termux/files/home/aithon/termuxOp")
