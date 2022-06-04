362ce87c0733be5811c8ef6419dbeb8d0f8a65e0
Py1000 <89375743+Py1000@users.noreply.github.com>
2022-06-04 14:13:12 +0530

    Update sdel.py

--- termuxPlug/sdel.py
+++ termuxPlug/sdel.py
@@ -5,4 +5,7 @@
 async def sdel(event):
   message = event.raw_text
   sec = (message.split(" "))[1]
-  
+  to_show = (message.split("|"))[1]
+  await event.edit(to_show)
+  asyncio.sleep(int(sec))
+  await event.delete()
